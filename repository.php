cdbdd862b982872eaea05ddae70a2c711de4b4bc
Andy Potanin <andy.potanin@usabilitydynamics.com>
2014-06-12 16:08:09 -0400

    [ci skip]

---
+++ repository.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Repository
+ * Plugin URI: http://twincitiestech.com/plugins/
+ * Description: -
+ * Author: Usability Dynamics, Inc.
+ * Version: 1.0
+ * Author URI: http://usabilitydynamics.com
+ *
+ */
