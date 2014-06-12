ed5b16ed63c6f671e24f0cfb31e44847eae2ffe9
Andy Potanin <andy.potanin@usabilitydynamics.com>
2014-06-12 18:00:35 -0400

    Automated commit. [ci skip]

--- repository.php
+++ repository.php
@@ -8,3 +8,50 @@
  * Author URI: http://usabilitydynamics.com
  *
  */
+
+// @legacy
+add_action( 'init', function() {
+
+  function time_ago( $time ) {
+
+  $_ago = round( ( ( time() - $time ) )  / 60 );
+
+  if( $_ago == 0 ) {
+    return 'less than a minute ago';
+  }
+
+  return $_ago . ' minutes ago';
+
+}
+
+  function get_includes() {
+
+    $_list = array();
+
+    foreach (glob("repository/*.json") as $filename) {
+
+      $_list[ 'repository/' . str_replace( '', '', basename( $filename ) ) ] = array(
+        'sha1' => sha1( filemtime( $filename ) ),
+        'updated' => filemtime( $filename ),
+        'description' => 'Updated ' . time_ago( filemtime( $filename ) ) . '.'
+      );
+
+    }
+
+    return $_list;
+
+  }
+
+  header( 'Cache-Control:no-cache' );
+  header( 'Content-Type:application/json' );
+  header( 'Last-Modified:Thu, 08 May 2014 22:01:01 GMT' );
+  header( 'Date:Thu, 08 May 2014 22:10:21 GMT' );
+
+  $_response = array(
+    "ok" => true,
+    "includes" => get_includes()
+  );
+
+  die( json_encode( $_response ) );
+
+});
