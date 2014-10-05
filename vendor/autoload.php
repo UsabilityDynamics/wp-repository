e6eac651de95cc7b4a2b60df7b444fb4208be02e
andypotanin <andy.potanin@usabilitydynamics.com>
2014-10-05 07:42:23 -0400

    [ci skip]

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInite2e481da9180173ab9568bfdab15b93e::getLoader();
+return ComposerAutoloaderInitbb97a2dca4584b977fb262a0ffb20a6f::getLoader();
